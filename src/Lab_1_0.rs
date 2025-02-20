3c87321230473fd4d9ad520ee58d0c186475a450
Виктор <victor.ar72@yandex.ru>
2025-02-20 20:35:00 +0300

    Загружена ЛР 1, версия 1.0

--- src/Lab_1_0.rs
+++ src/Lab_1_0.rs
@@ -1,3 +1,17 @@
-fn main(){
-    println!("Hello, Lab!")
+/* LR 1 version 1.0
+fn fib(n: u32) -> u32 {
+    // if (n < 2) {
+    if (n < 3) {
+        // первый шаг.
+        return 1;
+    } else {
+        // рекурсия.
+        return fib(n-1) + fib(n-2);
+    }
 }
+fn main() {
+    // Примечание
+    let n = 20;
+    println!("fib({n}) = {}", fib(n));
+}
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-fn main(){
-    println!("Hello, Lab!")
+fn fib(n: u32) -> u32 {
+    // if (n < 2) {
+    if n < 3 {
+        // первый шаг.
+        return 1;
+    } else {
+        // рекурсия.
+        return fib(n-1) + fib(n-2);
+    }
 }
-
+fn main() {
+    // Примечание - всё же корректнее считать, что и 2 число также равняется 1
+    // 0, 1, 1, 2, 3, 5, ...
+    let n :u32 = 22;
+    println!("fib({n}) = {}", fib(n));
+}
 
 
 //fn main() {
